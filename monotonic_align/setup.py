31cd3f10463ef0ba0bb2abcc1b876b1e5737a87b
Zackary Rackauckas <zrackauckas@gmail.com>
2026-06-12 13:36:54 -0700

    Fix monotonic align Windows build

--- monotonic_align/setup.py
+++ monotonic_align/setup.py
@@ -1,9 +1,17 @@
-from distutils.core import setup
+from setuptools import Extension, setup
 from Cython.Build import cythonize
 import numpy
 
+
+extensions = [
+  Extension(
+    name="core",
+    sources=["core.pyx"],
+    include_dirs=[numpy.get_include()],
+  )
+]
+
 setup(
-  name = 'monotonic_align',
-  ext_modules = cythonize("core.pyx"),
-  include_dirs=[numpy.get_include()]
+  name="monotonic_align",
+  ext_modules=cythonize(extensions),
 )
